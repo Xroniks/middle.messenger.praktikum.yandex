903a1ef65fa6e730983dd1525c761ee4f9eeb594
Xroniks <xroniks25@gmail.com>
2022-09-10 02:10:15 +0300

    исправлял в server.js

--- src/server.js
+++ src/server.js
@@ -1,21 +1,12 @@
 const fallback = require('express-history-api-fallback');
 const express = require('express');
 
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const root = './dist';
-app.use(express.static(root));
-app.use(fallback('index.html', { root }));
-
-// app.post('/api/form/save', function () {
-//   console.log('Запрос принят но не обработан :(');
-// })
-
-// app.post("/api/form/save", function (req, res) {
-//   if (!request.data) return response.sendStatus(400);
-//   response.json({ name: request.data.name, age: request.body.age });
-// });
+app.use(express.static(`/dist/`));
+app.use(fallback('./dist/index.html', {}));
 
 app.listen(PORT, () => {
   // eslint-disable-next-line
