f5337d95a6f13b6cd502410a149e90562469a774
Xroniks <Xroniks25@gmail.com>
2022-10-06 15:41:02 +0300

    Добавил докер, настроил сборку контейнера, выложил на хероку

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 const fallback = require('express-history-api-fallback');
 const express = require('express');
-
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const root = './dist';
+const root = path.resolve(__dirname, '../dist');
 app.use(express.static(root));
 app.use(fallback('index.html', { root }));
 
